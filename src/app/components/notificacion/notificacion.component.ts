77e6baf68a6982464109f6ec05d01057e9da99fa
ABOK451 <alancito2013america@gmail.com>
2025-11-03 20:37:22 -0600

    lo

--- src/app/components/notificacion/notificacion.component.ts
+++ src/app/components/notificacion/notificacion.component.ts
@@ -4,15 +4,11 @@ import { Component, Input, OnChanges } from '@angular/core';
   selector: 'app-notificacion',
   template: `
     <div *ngIf="visible" class="notificacion" [ngClass]="[tipo, showClass]">
-      <ng-container *ngIf="tipo === 'error'">
-        <h4 *ngIf="errores.length > 1">Se encontraron errores:</h4>
-        <ul *ngIf="errores.length > 0">
-          <li *ngFor="let e of errores">{{ e }}</li>
-        </ul>
-        <span *ngIf="errores.length === 1">{{ errores[0] }}</span>
+      <ng-container *ngIf="errores.length > 0">
+        <div *ngFor="let e of errores">{{ e }}</div>
       </ng-container>
 
-      <ng-container *ngIf="tipo === 'success'">
+      <ng-container *ngIf="tipo === 'success' && errores.length === 0">
         {{ mensaje }}
       </ng-container>
     </div>
@@ -29,9 +25,11 @@ export class NotificacionComponent implements OnChanges {
 
   ngOnChanges() {
     if (this.visible) {
-      // Separar los errores por salto de línea o coma
-      this.errores = this.tipo === 'error' ? this.mensaje.split(/\n|,/) : [];
+      // Separar los mensajes por salto de línea y eliminar duplicados
+      const mensajes = this.mensaje.split(/\n|,/).map(m => m.trim()).filter(m => m);
+      this.errores = Array.from(new Set(mensajes)); // quita duplicados
       this.showClass = 'show';
+
       // Ocultar automáticamente después de 5 segundos
       setTimeout(() => this.showClass = 'hide', 5000);
     }
