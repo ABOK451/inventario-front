d4a1d15a85d6f3a9b2a7cfc340ca977535220858
elizabethgutierrez27 <146129308+elizabethgutierrez27@users.noreply.github.com>
2025-11-03 18:47:47 -0600

    ok

--- src/app/components/barra/barra.component.ts
+++ src/app/components/barra/barra.component.ts
@@ -1,7 +1,6 @@
 import { AuthService } from './../../services/auth.service';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-barra',
